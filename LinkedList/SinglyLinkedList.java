65de20c397d4cc4ddf632de6582913b874e19f2b
Srikanth <sri1@publicisgroupe.net>
2022-03-23 14:53:39 +0530

    singly linked list deletion also added

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -68,6 +68,61 @@ public class SinglyLinkedList {
         return list;
     }
 
+    public static SinglyLinkedList deleteLast(SinglyLinkedList list) {
+        // check if there are no nodes
+        if (list.head == null) {
+            System.out.println("There is no linkedlist");
+            return list;
+        }
+
+        Node prevNode = null;
+        Node currentNode = list.head;
+        while (currentNode.next != null) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        prevNode.next = null;
+        System.out.println("The node deleted with the data: " + currentNode.data);
+        currentNode = null; // for garbage collector destory
+
+        return list;
+    }
+
+    public static SinglyLinkedList deleteFirst(SinglyLinkedList list) {
+        // check there is nodes or not
+        if (list.head == null) {
+            System.out.println("There is no linkedlist");
+            return list;
+        }
+
+        Node firstNode = list.head;
+        list.head = list.head.next;
+        System.out.println("The node deleted with the data: " + firstNode.data);
+        firstNode = null; // eligible for garbage collector 
+
+        return list;
+    }
+
+    public static SinglyLinkedList deleteAt(SinglyLinkedList list, int index) {
+        // check if this is first element
+        if (index == 0) {
+            list = deleteFirst(list);
+            return list;
+        }
+
+        Node prevNode = null;
+        Node currentNode = list.head;
+        for(int i=0; i<index; i++) {
+            prevNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        prevNode.next = currentNode.next;
+        System.out.println("The node deleted with the data: " + currentNode.data + " and index: " + index);
+        currentNode = null;
+
+        return list;
+    }
+
     public static void show(SinglyLinkedList list) {
         Node currentNode = list.head;
         if(list.head == null) {
@@ -103,10 +158,10 @@ public class SinglyLinkedList {
         SinglyLinkedList list = new SinglyLinkedList();
 
         // print list when nothing
-        list.show(list);
+        // list.show(list);
 
         // length of linkedlist
-        System.out.println("Linked list length: " + length(list));
+        // System.out.println("Linked list length: " + length(list));
 
         // insert last
         list = list.insertLast(list, 7);
@@ -122,6 +177,23 @@ public class SinglyLinkedList {
         list = list.insertAt(list, 55, 5);
         list = list.insertAt(list, 1111, 7);
         list = list.insertAt(list, 7611, 10);
+
+        // print list
+        list.show(list);
+
+        // length of linkedlist
+        System.out.println("Linked list length: " + list.length(list));
+        
+        // delete last node
+        // list = list.deleteLast(list);
+
+        // delete first node
+        // list = list.deleteFirst(list);
+
+        // delete at given position
+        list = list.deleteAt(list, 2);
+        list = list.deleteAt(list, 0);
+        list = list.deleteAt(list, 6);
 
         // print list
         list.show(list);
